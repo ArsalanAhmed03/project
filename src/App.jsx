c3adedb3d3a3d82624cf4bb39d999f60467d344d
Arsalan Ahmed <134292213+ArsalanAhmed03@users.noreply.github.com>
2025-03-08 10:04:32 +0500

    overflowissuefix2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Gallery from './components/Gallery';
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-hidden" >
       <Navbar />
       <Hero />
       <Competitions />
