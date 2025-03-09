c044b018f0ac92024e45b5d2ab2079c8eb6d45b7
Arsalan Ahmed <134292213+ArsalanAhmed03@users.noreply.github.com>
2025-03-09 17:42:00 +0500

    navbarchnages

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Competitions />
-      <AutoshowSection />
+      {/* <AutoshowSection /> */}
       <Gallery />
       <Register />
       <Contact />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,12 +28,13 @@ const Navbar = () => {
     <nav className="fixed w-full bg-white/30 backdrop-blur-md z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
-          <div className="flex-shrink-0 flex items-center">
+        <div className="flex-shrink-0 flex items-center space-x-2">
             <IEEEMainLogo />
-          </div>
-          <div className="flex-shrink-0 flex items-center">
             <IEEELogo />
           </div>
+
+          {/* <div className="flex-shrink-0 flex items-center">
+          </div> */}
           <div className="hidden md:flex items-center space-x-4">
             {navItems.map((item) => (
               <Link
